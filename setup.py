80a0802cea3d75c7dc19f4f8f71dd18c30ede09b
Boris Zubarev <bobazooba@gmail.com>
2023-11-13 16:10:57 +0400

    Release: 0.0.2

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ install_requires = [
 # Setup
 setup(
     name="xllm",
-    version="0.0.1",
+    version="0.0.2",
     description="Simple & Cutting Edge LLM Finetuning",
     license_files=["LICENSE"],
     long_description=open("README.md", "r", encoding="utf-8").read(),
--- src/xllm/__init__.py
+++ src/xllm/__init__.py
@@ -14,7 +14,7 @@
 
 # ruff: noqa: F401
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from . import enums, types
 from .cli.fuse import cli_run_fuse
