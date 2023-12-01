0108586c6ac38d2796026b78823099bef8cddc63
Boris Zubarev <bobazooba@gmail.com>
2023-12-01 18:57:18 +0400

    add more tests

--- tests/unit/core/test_config.py
+++ tests/unit/core/test_config.py
@@ -12,9 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import pytest
 import torch
 
+from src.xllm import enums
 from src.xllm.core.config import Config
 from src.xllm.core.deepspeed_configs import STAGE_2
 
@@ -92,3 +95,43 @@ def test_check_auto_gptq_exception():
     config = Config()
     with pytest.raises(ImportError):
         config.check_auto_gptq()
+
+
+@pytest.mark.parametrize("huggingface_hub_token", ["some_huggingface_hub_token", "another_huggingface_hub_token"])
+def test_config_post_init_hf_token(huggingface_hub_token: str):
+    config = Config(huggingface_hub_token=huggingface_hub_token)
+    assert os.environ[enums.EnvironmentVariables.huggingface_hub_token] == huggingface_hub_token
+    assert os.environ[enums.EnvironmentVariables.huggingface_hub_token] == config.huggingface_hub_token
+
+
+@pytest.mark.parametrize("wandb_api_key", ["some_wandb_api_key", "another_wandb_api_key"])
+@pytest.mark.parametrize("wandb_project", ["some_wandb_project", "another_wandb_project"])
+@pytest.mark.parametrize("wandb_entity", [None, "some_wandb_entity", "another_wandb_entity"])
+def test_config_post_init_wandb_report(wandb_api_key: str, wandb_project: str, wandb_entity: str):
+    config = Config(
+        report_to_wandb=True,
+        wandb_api_key=wandb_api_key,
+        wandb_project=wandb_project,
+        wandb_entity=wandb_entity,
+    )
+
+    assert os.environ[enums.EnvironmentVariables.wandb_api_key] == wandb_api_key
+    assert os.environ[enums.EnvironmentVariables.wandb_api_key] == config.wandb_api_key
+
+    assert os.environ[enums.EnvironmentVariables.wandb_project] == wandb_project
+    assert os.environ[enums.EnvironmentVariables.wandb_project] == config.wandb_project
+
+    if wandb_entity is not None:
+        assert os.environ[enums.EnvironmentVariables.wandb_entity] == wandb_entity
+        assert os.environ[enums.EnvironmentVariables.wandb_entity] == config.wandb_entity
+
+
+@pytest.mark.parametrize("master_port", [0, 8080, 9999])
+def test_config_apply_deepspeed_single_gpu(master_port: int):
+    config = Config(single_gpu=True, master_port=master_port)
+    config.apply_deepspeed_single_gpu()
+    assert os.environ[enums.EnvironmentVariables.master_address] == "localhost"
+    assert os.environ[enums.EnvironmentVariables.master_port] == str(master_port)
+    assert os.environ[enums.EnvironmentVariables.rank] == "0"
+    assert os.environ[enums.EnvironmentVariables.local_rank] == "0"
+    assert os.environ[enums.EnvironmentVariables.world_size] == "1"
